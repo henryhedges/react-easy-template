1f4025c06c98ff217a1e6d1533762a668457e4c1
Henry Hedges <henryhedges1@gmail.com>
2017-05-17 20:49:00 -0500

    reorganized files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './App/App.js';
+import './assets/styles/index.css';
+
+// This is what you use to append react to your application html
 
 ReactDOM.render(
   <App />,
